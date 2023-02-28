0a0de62f92ee8ce30ce105b7c1308a7d56950419
Mariia <milonigame1@gmail.com>
2023-02-28 13:45:18 +0300

    описание статуса

--- calc/js/script.js
+++ calc/js/script.js
@@ -9,5 +9,16 @@ inputRub.addEventListener('input', ()=>{
     //request.open(method, url, async, login, pass);
     request.open('GET', 'js/curent.json');
     request.setRequestHeader('Content-type','application/json;charset=utf-8')
+    request.send();
+
+
+    //status-какой статус,например 404 или 101 и тд.
+    //statusText-текстовое описание ответа от сервера
+    //response -ответ от сервера,
+    //тот ответ который нам задал бэкенд разработчик
+    //readyState-текущее состояние запроса
+
     
+
+
 });
