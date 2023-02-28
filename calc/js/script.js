e835280d52217218c98d31666f9af1662376357a
Mariia <milonigame1@gmail.com>
2023-02-28 13:33:17 +0300

    open and setRequestHeade

--- calc/js/script.js
+++ calc/js/script.js
@@ -1,4 +1,13 @@
 'use strict';
 
-const inputRub=document.querySelector('#rub');
+const inputRub=document.querySelector('#rub'),
       inputUsd=document.querySelector('#usd'); 
+
+inputRub.addEventListener('input', ()=>{
+    const request=new XMLHttpRequest();
+
+    //request.open(method, url, async, login, pass);
+    request.open('GET', 'js/curent.json');
+    request.setRequestHeader('Content-type','application/json;charset=utf-8')
+    
+});
